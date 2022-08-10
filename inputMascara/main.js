f99eada9bfc85adc69708a2c216fe6ff7e7bd5b8
jesusLayton <jesuslayton92@gmail.com>
2022-08-09 21:48:10 -0500

    javascript

---
+++ inputMascara/main.js
@@ -0,0 +1,37 @@
+const inputCard = document.querySelector('#inputCard');
+const inputDate = document.querySelector('#inputDate');
+const inputCVV = document.querySelector('#inputCVV');
+
+const maskNumber = '####-####-####-####';
+const maskDate = '##/##';
+const maskCVV = '###';
+
+let current ="";
+let cardNumber = [];
+let dataNumber = [];
+let cvvNumber = [];
+
+inputCard.addEventListener('keydown', (e)=>{
+    if (e.key === 'Tab') {
+        return;        
+    }
+    e.preventDefault();
+    handleInput(maskNumber, e.key, cardNumber);
+    inputCard.value = cardNumber.join("");
+    
+});
+
+function handleInput(mask, key, arr){
+    let numbers = ["0", "1", "2","3", "4", "5","6", "7", "8", "9"];
+
+    if (key === 'Backspace' && arr.length > 0) {
+        arr.pop();
+        return;
+    } if(numbers.includes(key) && arr.length +a <= mask.length  ) {
+        if (mask[arr.length]) {
+            
+        }
+        
+    }
+
+}
